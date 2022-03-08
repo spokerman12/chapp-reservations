de8fe0d04f79696ffa69f373a5d0bbcf57ed345c
Daniel Francis <spokerman12@hotmail.com>
2022-03-08 12:35:27 +0100

    fix: serve statics

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,12 @@
 from . import views
+from django.views.static import serve
 
 from django.contrib import admin
-from django.urls import include, path
+from django.conf import settings
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path('', include('reservations.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
