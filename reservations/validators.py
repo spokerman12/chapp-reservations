03b52c33b4b583cdfc45901c2bc85a980831eed8
Daniel Francis <spokerman12@hotmail.com>
2022-03-10 10:13:53 +0100

    fix: more parsing

--- reservations/validators.py
+++ reservations/validators.py
@@ -8,6 +8,13 @@ TODAY = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
 TOMORROW = TODAY + timedelta(days=1)
 
 
+def convert_dt(date_str):
+    """
+    Convierte una fecha "%Y-%m-%d" en datetime.date
+    """
+    return datetime.strptime(date_str, "%Y-%m-%d").date()
+
+
 def validate_gte_zero(value):
     """
     Verifica que value >= 0
@@ -28,7 +35,7 @@ def validate_check_in(value):
     Verifica que value no sea en el pasado
     """
     if type(value) == str:
-        value = datetime.strptime(value, "%Y-%m-%d")
+        value = convert_dt(value)
     if value < TODAY:
         raise ValidationError(
             f"Check-in date {value} must be at least today.",
@@ -40,7 +47,7 @@ def validate_check_out(value):
     Verifica que value sea al menos mañana
     """
     if type(value) == str:
-        value = datetime.strptime(value, "%Y-%m-%d")
+        value = convert_dt(value)
     if value < TOMORROW:
         raise ValidationError(
             f"Check-out date {value} must be at least tomorrow.",
@@ -52,7 +59,7 @@ def validate_current_year(value):
     Verifica que value sea en el año en curso
     """
     if type(value) == str:
-        value = datetime.strptime(value, "%Y-%m-%d").year
+        value = convert_dt(value).year
     if value != datetime.today().year:
         raise ValidationError(
             f"Please select a date in {datetime.today().year}",
@@ -65,9 +72,9 @@ def validate_check_in_out(check_in, check_out):
     """
 
     if type(check_in) == str:
-        check_in = datetime.strptime(check_in, "%Y-%m-%d")
+        check_in = convert_dt(check_in)
     if type(check_out) == str:
-        check_out = datetime.strptime(check_out, "%Y-%m-%d")
+        check_out = convert_dt(check_out)
 
     if check_in >= check_out:
         raise ValidationError(
